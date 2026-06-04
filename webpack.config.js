97aa640f1a316285625716393d3091e6ce94239a
Erik Watson <championchap@gmail.com>
2026-06-05 00:40:41 +0100

    fixing the images preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
+  mode: "development",
+  entry: "./src/index.tsx",
   plugins: [
-    new HtmlWebpackPlugin({ template: './docs/template.html' })
+    new HtmlWebpackPlugin({ template: "./assets/template.html" }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "assets/images",
+          to: "assets",
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx', '.css'],
+    extensions: [".ts", ".js", ".tsx", ".jsx", ".css"],
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'docs'),
+    filename: "app.js",
+    path: path.resolve(__dirname, "docs"),
     clean: true,
   },
   devServer: {
-    static: path.resolve(__dirname, 'docs'),
-    hot: true,              // enables hot module replacement (HMR)
-    watchFiles: ['src/**/*', 'docs/template.html'], // ensures CSS/template changes trigger reload
+    static: path.resolve(__dirname, "docs"),
+    hot: true, // enables hot module replacement (HMR)
+    watchFiles: ["src/**/*", "assets/**/*"], // ensures CSS/template changes trigger reload
     port: 3000,
   },
-  devtool: 'source-map',
+  devtool: "source-map",
 };
